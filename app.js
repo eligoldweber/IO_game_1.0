75607f972e762beca2a9cfb3a704558d12d34342
eli goldweber <eligoldweber@gmail.com>
2016-12-26 16:02:36 -0500

    init

--- app.js
+++ app.js
@@ -55,8 +55,9 @@ var Player = function(id,name){
 		super_update();
 		
 		if(self.pressingAttack){
-			for(var i = -3; i<3;i++)
-				self.shootBullet(i * 10 + self.mouseAngle);
+			// for(var i = -3; i<3;i++)
+// 				self.shootBullet(i * 10 + self.mouseAngle);
+			self.shootBullet(self.mouseAngle);
 		}
 
 	}
